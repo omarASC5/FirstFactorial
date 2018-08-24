a85f934c78fbd8d8f327bc9e66ad37fdcb2d628c
omarASC5 <41080197+omarASC5@users.noreply.github.com>
2018-08-23 21:09:19 -0400

    Create FirstFactorial.js

---
+++ FirstFactorial.js
@@ -0,0 +1,33 @@
+function FirstFactorial(num) { 
+
+  // code goes here  
+  let count = num;
+  while (count > 0) {
+      count = count - 1;
+      num += (num * count);
+  }
+  return num; 
+         
+}
+   
+// keep this function call here 
+FirstFactorial(readline());                            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                            
+                            
+                            
+  
